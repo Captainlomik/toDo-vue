17cd963831004dc7b75ba602f95bacfde0ad1699
captainlomik <nastiy248@mail.ru>
2023-05-21 00:01:39 +0300

    add scss

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 import components from '@/components/UI/index'
 import router from './router/router'
-import 'vuetify/dist/vuetify.min.css'
 
-loadFonts()
 const app = createApp(App)
 components.forEach(el => {
     app.component(el.name, el)
 })
 
-app.use(vuetify)
 app.use(router)
 app.mount('#app')
 
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
 })
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: ` @import "./src/scss/_variables.scss";`
+      }
+    }
+  }
+};
