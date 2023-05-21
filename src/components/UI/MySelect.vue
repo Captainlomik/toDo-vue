3227b7be0e8e98fcf5992ecea43b5d74d7cd3210
captainlomik <nastiy248@mail.ru>
2023-05-21 12:07:57 +0300

    add select

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,33 @@
+<template>
+    <select name="" id="">
+        <option default>Выберите предмет</option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'my-select'
+}
+</script>
+
+<style lang="scss" scoped>
+select {
+    width: 60%;
+    min-width: 320px;
+    border-radius: $radius;
+    padding: 20px;
+    border: none;
+    color: #171B0980;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 27px;
+    margin-right: 30px;
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    background: url('@/assets/arrow.svg') no-repeat;
+    background-color: $gray;
+    background-position: 90% 50%;
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,7 +1,9 @@
 import MyNavbar from '@/components/UI/MyNavbar'
 import MyButton from '@/components/UI/MyButton'
+import MySelect from '@/components/UI/MySelect'
 
 export default [
     MyNavbar,
-    MyButton
+    MyButton,
+    MySelect,
 ]
