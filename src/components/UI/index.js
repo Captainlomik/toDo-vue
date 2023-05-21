fd0ab9188335a7f348bcc6f22166d892eaae4d3e
captainlomik <nastiy248@mail.ru>
2023-05-21 15:09:45 +0300

    add small btn

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,9 +1,11 @@
 import MyNavbar from '@/components/UI/MyNavbar'
 import MyButton from '@/components/UI/MyButton'
+import MySmallButton from '@/components/UI/MySmallButton'
 import MySelect from '@/components/UI/MySelect'
 
 export default [
     MyNavbar,
     MyButton,
     MySelect,
+    MySmallButton
 ]
